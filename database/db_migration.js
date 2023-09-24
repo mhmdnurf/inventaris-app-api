fb850ae9ef1e736d1fb2bde88528570a45a85be6
mhmdnurf <mhmd.nurfat@gmail.com>
2023-09-24 23:32:33 +0700

    setup for mongodb

--- database/db_migration.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("inventaris_app", "root", "", {
-  host: "localhost",
-  dialect: "mariadb",
-});
-
-module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 const express = require("express");
-const cors = require("cors");
-const port = 3001;
-const sequelize = require("./database/db_migration");
-const barangEndpoint = require("./routes/barang");
-const bahanEndpoint = require("./routes/bahan");
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Sequelize connected to database");
-  })
-  .catch((err) => {
-    console.log("Sequelize failed to connect to database:", err);
-  });
-sequelize.sync().then(() => {
-  console.log("Database is ready!");
-});
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(express.static(__dirname));
-app.use(cors());
-app.use(express.json());
+const port = 3000;
 
-app.use("/master-barang", barangEndpoint);
-app.use("/master-bahan", bahanEndpoint);
+const mongoURI = "mongodb://127.0.0.1:27017/db_inventaris";
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Mongoose");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to Mongoose");
+  });
 
 app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
